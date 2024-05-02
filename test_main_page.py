baa9abecaee1210f4c13a98d2816a10e673f6116
Elena <el.garaseva@gmail.com>
2024-05-02 16:41:19 +0300

    refactor test_guest_can_go_to_login_page by Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,8 @@
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
