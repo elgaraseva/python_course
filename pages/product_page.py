f43cb693cc183620aec61715b6d90693cb30eabc
Elena <el.garaseva@gmail.com>
2024-05-04 23:07:55 +0300

    new tests for product page, refactoring

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,16 +3,18 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
+    def add_value_to_popup(self):
+        BasePage.solve_quiz_and_get_code(self)
+
     def click_button_add_to_basket(self):
         button_add_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         button_add_to_basket.click()
 
-    def add_value_to_popup(self):
-        BasePage.solve_quiz_and_get_code(self)
-
     def prepare_expected_result(self, field_name=""):
-        if field_name == "name": expected_result = self.browser.find_element(*ProductPageLocators.EXPECTED_NAME_OF_BOOK).text
-        if field_name == "price": expected_result = self.browser.find_element(*ProductPageLocators.EXPECTED_PRICE_OF_BOOK).text
+        if field_name == "name": expected_result = self.browser.find_element(
+            *ProductPageLocators.EXPECTED_NAME_OF_BOOK).text
+        if field_name == "price": expected_result = self.browser.find_element(
+            *ProductPageLocators.EXPECTED_PRICE_OF_BOOK).text
         return expected_result
 
     def should_be_expected_item_in_basket(self, actual_name="", actual_price=""):
@@ -22,21 +24,24 @@ class ProductPage(BasePage):
         self.should_be_price_of_book(expected_price=actual_price)
 
     def should_be_message_book(self):
-        assert self.is_element_present(*ProductPageLocators.MESSAGE_BOOK_IN_BASKET), 'Message about adding book to basket is not presented'
+        assert self.is_element_present(
+            *ProductPageLocators.MESSAGE_BOOK_IN_BASKET), 'Message about adding book to basket is not presented'
 
     def should_be_name_of_book(self, expected_name):
         actual_name = self.browser.find_element(*ProductPageLocators.ACTUAL_NAME_OF_BOOK).text
         assert actual_name == expected_name, f"The book {actual_name} in basket doesn't equal {expected_name}"
 
     def should_be_message_price(self):
-        assert self.is_element_present(*ProductPageLocators.MESSAGE_PRICE_OF_BASKET), 'Message about price in basket is not presented'
+        assert self.is_element_present(
+            *ProductPageLocators.MESSAGE_PRICE_OF_BASKET), 'Message about price in basket is not presented'
 
     def should_be_price_of_book(self, expected_price):
         actual_price = self.browser.find_element(*ProductPageLocators.ACTUAL_PRICE_OF_BOOK).text
         assert actual_price == expected_price, f"The price {actual_price} in basket doesn't equal {expected_price}"
 
     def should_not_be_success_message(self):
-        assert not self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+        assert not self.is_not_element_present(
+            *ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
 
     def should_not_be_success_message_not_disappeared(self):
         assert not self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is not disappeared"
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,6 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+
 import pytest
 
 
@@ -13,7 +15,7 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 @pytest.mark.skip
-def test_add_book_to_basket(browser, link):
+def test_guest_can_add_book_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     name = page.prepare_expected_result("name")
@@ -21,6 +23,21 @@ def test_add_book_to_basket(browser, link):
     page.click_button_add_to_basket()
     page.add_value_to_popup()
     page.should_be_expected_item_in_basket(name, price)
+
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+def test_guest_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
 
